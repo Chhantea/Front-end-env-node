e183a7d7d4c75f8bc403b1f2cab342127fd4f3ec
Chhantea <r.lalnghenchhana@gmail.com>
2018-12-13 15:55:19 +0530

    crud with axios and router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
-
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
 ReactDOM.render(
-    <App />,document.getElementById('root'));
+    <Router>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route path='/edit/:id' component={Edit} />
+            <Route path='/create' component={Create} />
+            <Route path='/show/:id' component={Show} />
+        </div>
+    </Router>
+    ,document.getElementById('root'));
