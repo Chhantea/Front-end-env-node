7a9d0e80d7df38ed39451308107516e2ecaea57f
Chhantea <r.lalnghenchhana@gmail.com>
2018-12-14 17:16:21 +0530

    clean ups

--- src/App.js
+++ src/App.js
@@ -1,52 +1,9 @@
 import React,{Component} from 'react';
-import axios from 'axios';
-import ItemTable from './components/ItemTable';
-import NewForm from './components/NewForm';
-class App extends Component{
-    constructor(props){
-     super(props);
-     this.state = {
-         items: []
-      };
-        // this.handleAdd = this.handleAdd.bind(this);
-        // this.handleDeleteRecord = this.handleDeleteRecord.bind(this);
-       // if ve use functionname = (somevalue) => {} we dont need to bind like on the above
-    }
-    componentDidMount(){
-      axios.get('http://localhost:3000/api/item')
-          .then((res) =>{
-              this.setState({items: res.data});
-          });
-    }
-    handleAdd = (item) => {
-        //error
-        var items = this.state.items;
-        items.push(item);
-        this.setState({items: items});
 
-    }
-    handleDeleteRecord = (item) => {
-        var items = this.state.items.slice();
-        var index = items.indexOf(item);
-        items.splice(index, 1);
-        this.setState({items: items});
-    }
-    handleUpdateRecord = (old_item, item) =>{
-        var items = this.state.items.slice();
-        var index = items.indexOf(old_item);
-        items.splice(index, 1, item);
-        this.setState({items: items});
-    }
+class App extends Component{
     render(){
         return(
-            <div className='jumbotron'>
-                <div className=''>
-                    <NewForm handleAdd = {this.handleAdd}/>
-                </div>
-                <ItemTable items={this.state.items}
-                           handleDeleteRecord={this.handleDeleteRecord}
-                           handleUpdateRecord={this.handleUpdateRecord} />
-            </div>
+            <div className='container'>Hello world</div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReacDOM from 'react-dom';
 import App from "./App";
 
 ReacDOM.render(
-    <App/>,document.getElementById('tablecrud'));
+    <App/>,document.getElementById('root'));
