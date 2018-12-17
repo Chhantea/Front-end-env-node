1ec245ce9c3d15ec960c886c94b89d63aa9c6248
Chhantea <r.lalnghenchhana@gmail.com>
2018-12-17 14:44:05 +0530

    user table working on

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React,{Component} from 'react';
+import axios from 'axios';
+import UserTable from './components/UserTable'
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            users: []
+        }
+    }
+    componentDidMount(){
+        axios.get('http://localhost:3000/api/user')
+            .then(res =>{
+               this.setState({users: res.data});
+               console.log("get data", res.data);
+            });
+    }
+    handleUpdateRecord = (old_user, user) =>{
+        var users = this.state.users.slice();
+        var index = users.indexOf(old_user);
+        users.splice(index, 1, user);
+        this.setState({users: users});
+    }
+
     render(){
         return(
-            <div className='container'>Hello world</div>
+            <div>
+                <UserTable users={this.state.users}
+                           handleUpdateRecord={this.handleUpdateRecord}/>
+            </div>
         )
     }
 }
---
+++ src/components/UserTable.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import User from './user'
+class UserTable extends Component{
+
+    render() {
+        var users = [];
+        this.props.users.forEach(function (user) {
+            users.push(<User user={user}
+                             key={user + user._id}
+                             handleUpdateRecord={this.props.handleUpdateRecord}
+                               /> );
+        }.bind(this));
+        return(
+            <table className='table table-bordered table-dark table-hover '>
+                <thead className=''>
+                <tr>
+                    <th>Id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>User Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>DOB</th>
+                    <th>Account Type</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                 {users}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default UserTable;
