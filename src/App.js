29375033598ed12cea9297a348f620eff723e0ab
Chhantea <r.lalnghenchhana@gmail.com>
2018-12-18 15:43:13 +0530

    restore

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React,{Component} from 'react';
 import axios from 'axios';
-import UserTable from './components/UserTable'
 
-class App extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            users: []
-        }
-    }
-    componentDidMount(){
-        axios.get('http://localhost:3000/api/user')
-            .then(res =>{
-               this.setState({users: res.data});
-                var strftime = require('strftime');
-               console.log("get data",strftime('%d %B, %Y %H:%M:%S'));
-            });
-    }
-    handleUpdateRecord = (old_user, user) =>{
-        var users = this.state.users.slice();
-        var index = users.indexOf(old_user);
-        users.splice(index, 1, user);
-        this.setState({users: users});
-    }
-
+class App extends Component {
     render(){
         return(
-            <div>
-                <UserTable users={this.state.users}
-                           handleUpdateRecord={this.handleUpdateRecord}/>
-            </div>
-        )
+          <div>
+              some
+          </div>
+        );
     }
 }
-
 export default App;
